6e5c091b91ea5604c809ba598d30b29aa44d3452
The Tech Gent <dalegent@hotmail.com>
2025-04-18 20:56:48 +0100

    Finished writing Quick Sort algorithm - needs refining

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -2,19 +2,51 @@
 
 using namespace std;
 
-// O(n log n)
+// Algorithm Quick Sort, Big O = O(n log n)
 
-int Partition()
+int Partition(int arr[], int low, int high)
 {
-
+	/*
+	What is happening here?
+	*/
+	int pivot = arr[high]; // choosing the last element as pivot
+	int i = low - 1; // index of smaller element
+	
+	for (int j = low; j < high; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++;
+			swap(arr[i], arr[j]);
+		}
+	}
+	
+	swap(arr[i + 1], arr[high]);
+	
+	return i + 1;
 }
 
-void QuickSort()
+void QuickSort(int arr[], int low, int high)
 {
-
+	if (low < high)
+	{
+		int pi = Partition(arr, low, high);
+		QuickSort(arr, low, pi - 1);
+		QuickSort(arr, pi + 1, high);
+	}
 }
 
 int main()
 {
-	
+	int arr[] = { 10, 7, 8, 9, 1, 5 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	QuickSort(arr, 0, n - 1);
+
+	cout << "Sorted array: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	return 0;
 }
